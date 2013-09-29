f67ca9690300c245023994959f82529d9b2e630e
Stefan Scherer <scherer_stefan@icloud.com>
2013-09-29 04:21:42 +0200

    pin a program to taskbar

--- win2008r2/shell/pin.ps1
+++ win2008r2/shell/pin.ps1
@@ -1,9 +1,3 @@
-& cinst VisualStudio2012Professional 
-& cinst Dogtail.VS2012.3
-& cinst visualsvn
-& cinst wixtoolset 
-
-"Pin VS2012 to taskbar"
 $shell = New-Object -ComObject Shell.Application
 $programsFolder = $shell.Namespace(23).Self.Path
 $vsfolder = $shell.Namespace($programsFolder + "\Microsoft Visual Studio 2012")

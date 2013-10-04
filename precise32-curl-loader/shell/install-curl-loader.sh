b8291b1cf367a036f57495bb2f612c307654a9d3
Stefan Scherer <scherer_stefan@icloud.com>
2013-10-04 19:32:35 +0200

    apt-get unattended, use option -y

--- precise32-curl-loader/shell/install-curl-loader.sh
+++ precise32-curl-loader/shell/install-curl-loader.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install git make
+sudo apt-get -y install git make
 git clone https://github.com/StefanScherer/curl-loader
 sudo chown -R vagrant:vagrant curl-loader
 cd curl-loader

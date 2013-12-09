4bf111f0a909b24e62fb96d4954e603f5a1748f1
Stefan Scherer <scherer_stefan@icloud.com>
2013-12-09 21:43:41 +0100

    some more npm installs

--- ubuntu1310/shell/install-tools.sh
+++ ubuntu1310/shell/install-tools.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 sudo apt-get -y install compizconfig-settings-manager
 sudo apt-get -y install vim git
-sudo apt-get -y install nodejs npm
-npm install -g yo
+sudo apt-get -y install nodejs nodejs-legacy npm
+sudo npm install -g yo
+sudo npm install -g generator-webapp
+sudo npm install -g generator-angular
 

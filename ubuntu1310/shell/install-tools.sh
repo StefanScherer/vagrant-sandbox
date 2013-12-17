47d84daf1654b06cc8cd02568f9f3797df5bceec
Stefan Scherer <scherer_stefan@icloud.com>
2013-12-17 11:25:19 +0100

    initialize ubuntu box with own node project

--- ubuntu1310/shell/install-tools.sh
+++ ubuntu1310/shell/install-tools.sh
@@ -6,3 +6,9 @@ sudo npm install -g yo
 sudo npm install -g generator-webapp
 sudo npm install -g generator-angular
 
+echo "vagrant soft nofile 80000" | sudo tee -a /etc/security/limits.conf
+echo "vagrant hard nofile 80000" | sudo tee -a /etc/security/limits.conf
+
+echo "export GIT_EDITOR=vim" >> /home/vagrant/.bashrc
+
+git clone https://StefanScherer@bitbucket.org/StefanScherer/node-netserver-test.git

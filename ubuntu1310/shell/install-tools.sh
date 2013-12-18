01847e26b04c0f0f73984afac4350fec87bd0cf7
Stefan Scherer <scherer_stefan@icloud.com>
2013-12-18 18:18:45 +0100

    install express, set some git globals

--- ubuntu1310/shell/install-tools.sh
+++ ubuntu1310/shell/install-tools.sh
@@ -5,6 +5,11 @@ sudo apt-get -y install nodejs nodejs-legacy npm
 sudo npm install -g yo
 sudo npm install -g generator-webapp
 sudo npm install -g generator-angular
+sudo npm install -g express
+
+git config --global user.name "StefanScherer"
+git config --global user.email scherer_stefan@icloud.com
+git config --global credential.helper cache
 
 echo "vagrant soft nofile 80000" | sudo tee -a /etc/security/limits.conf
 echo "vagrant hard nofile 80000" | sudo tee -a /etc/security/limits.conf

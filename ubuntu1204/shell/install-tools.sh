82e8ecdcb3df88dc3d9442f93121cd8cf648bf51
Stefan Scherer <scherer_stefan@icloud.com>
2014-01-09 00:12:31 +0100

    ubuntu 12.04.3 (minimal) desktop

---
+++ ubuntu1204/shell/install-tools.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+sudo apt-get -y install compizconfig-settings-manager
+sudo apt-get -y install vim git
+sudo apt-get -y install nodejs nodejs-legacy npm
+sudo npm install -g yo
+sudo npm install -g generator-webapp
+sudo npm install -g generator-angular
+sudo npm install -g express
+
+git config --global user.name "StefanScherer"
+git config --global user.email scherer_stefan@icloud.com
+git config --global credential.helper cache
+
+echo "vagrant soft nofile 80000" | sudo tee -a /etc/security/limits.conf
+echo "vagrant hard nofile 80000" | sudo tee -a /etc/security/limits.conf
+
+echo "export GIT_EDITOR=vim" >> /home/vagrant/.bashrc
+
+# git clone https://StefanScherer@bitbucket.org/StefanScherer/node-netserver-test.git

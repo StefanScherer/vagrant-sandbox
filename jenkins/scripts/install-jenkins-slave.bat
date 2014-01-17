5bde909dc2f1129c1ac8213439254b842381bb75
Stefan Scherer <scherer_stefan@icloud.com>
2014-01-18 00:18:28 +0100

    schedule task for swarm client

--- jenkins/scripts/install-jenkins-slave.bat
+++ jenkins/scripts/install-jenkins-slave.bat
@@ -1,8 +1,11 @@
 rem @echo off
 echo Ensuring .NET 4.0 is installed
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.github.com/StefanScherer/arduino-ide/install/InstallNet4.ps1'))"
-echo Installing Chocolatey
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+where cinst
+if ERRORLEVEL 1 (
+  echo Installing Chocolatey
+  @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+)
 
 where cinst
 if ERRORLEVEL 1 goto set_chocolatey
@@ -11,14 +14,33 @@ goto inst
 set ChocolateyInstall=%SystemDrive%\Chocolatey
 set PATH=%PATH%;%ChocolateyInstall%\bin
 :inst
-call cinst git
-call cinst javaruntime
-call cinst wget
-set JENKINSHOST=192.168.33.214
-call wget http://%JENKINSHOST%:8080/jnlpJars/slave.jar
-where java
 
-java -jar slave.jar -jnlpUrl http://%JENKINSHOST%:8080/computer/slave1/slave-agent.jnlp
-where javaws
-rem javaws http://%JENKINSHOST%:8080/computer/slave1/slave-agent.jnlp
+call cinst vim
+
+where java
+if ERRORLEVEL 1 call cinst jdk7
+rem it is reproducible that first cinst jdk7 fails, so try again if java is still not in path
+where java
+if ERRORLEVEL 1 call cinst jdk7
+where java
+if ERRORLEVEL 1 call cinst jdk7
+
+where wget
+if ERRORLEVEL 1 call cinst wget
+
+if not exist c:\jenkins mkdir c:\jenkins
+
+type c:\vagrant\resources\jenkins-host.txt >> c:\Windows\System32\drivers\etc\hosts
+for /f "tokens=2" %%i in ('findstr 1 c:\vagrant\resources\jenkins-host.txt') do set jenkinshost=%%i
+
+if not exist c:\jenkins\swarm-client.jar (
+  copy /y c:\vagrant\resources\swarm-client.jar c:\jenkins
+)
+
+rem set Internet Explorer start page to jenkins
+reg add "HKCU\Software\Microsoft\Internet Explorer\Main" /v "Start Page" /d "http://%jenkinshost%" /f
+
+rem Due to problems with UDP broadcast, use the -master switch at the moment
+rem Schedule start of swarm client at start of the machine (after next reboot)
+schtasks /CREATE /SC ONSTART /RU vagrant /RP vagrant /TN JenkinsSwarmClient /TR "java.exe -jar c:\jenkins\swarm-client.jar -master http://%jenkinshost% -labels windows -fsroot c:\jenkins"
 

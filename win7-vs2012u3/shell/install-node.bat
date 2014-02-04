5384fdc361d1034676ae16d6ec66d1e32f26cbad
Stefan Scherer <scherer_stefan@icloud.com>
2014-02-04 18:05:11 +0100

    nodejs.install has npm inside

--- win7-vs2012u3/shell/install-node.bat
+++ win7-vs2012u3/shell/install-node.bat
@@ -1,7 +1,8 @@
-call cinst nodejs.commandline
+rem call cinst nodejs.commandline
+call cinst nodejs.install
+call cinst curl
 call cinst Yeoman
 call cinst SublimeText3
 call cinst GoogleChrome
 powershell powershell -NoProfile -ExecutionPolicy Bypass -File %~dp0\PinChrome.ps1
-call cinst curl
 call cinst gb.MongoDB 

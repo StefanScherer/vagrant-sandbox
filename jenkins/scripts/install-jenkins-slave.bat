e0635f25a71f58ea252b3179b13723129a717ebd
StefanScherer <scherer_stefan@icloud.com>
2014-03-09 13:03:11 +0100

    xml heredoc in bat

--- jenkins/scripts/install-jenkins-slave.bat
+++ jenkins/scripts/install-jenkins-slave.bat
@@ -46,5 +46,81 @@ if not exist c:\jenkins\swarm-client.jar (
   )
 )
 
+call :heredoc html >%TEMP%\JenkinsSwarmClient.xml && goto next2
+<?xml version="1.0" encoding="UTF-16"?>
+<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+  <RegistrationInfo>
+    <Date>2014-03-09T01:02:10</Date>
+    <Author>vagrant</Author>
+  </RegistrationInfo>
+  <Triggers>
+    <BootTrigger>
+      <StartBoundary>2014-03-09T01:02:00</StartBoundary>
+      <Enabled>true</Enabled>
+    </BootTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id="Author">
+      <UserId>vagrant</UserId>
+      <LogonType>Password</LogonType>
+      <RunLevel>LeastPrivilege</RunLevel>
+    </Principal>
+  </Principals>
+  <Settings>
+    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
+    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>false</StopIfGoingOnBatteries>
+    <AllowHardTerminate>true</AllowHardTerminate>
+    <StartWhenAvailable>false</StartWhenAvailable>
+    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
+    <IdleSettings>
+      <StopOnIdleEnd>true</StopOnIdleEnd>
+      <RestartOnIdle>false</RestartOnIdle>
+    </IdleSettings>
+    <AllowStartOnDemand>true</AllowStartOnDemand>
+    <Enabled>true</Enabled>
+    <Hidden>false</Hidden>
+    <RunOnlyIfIdle>false</RunOnlyIfIdle>
+    <WakeToRun>false</WakeToRun>
+    <ExecutionTimeLimit>PT0S</ExecutionTimeLimit>
+    <Priority>7</Priority>
+    <RestartOnFailure>
+      <Interval>PT1M</Interval>
+      <Count>3</Count>
+    </RestartOnFailure>
+  </Settings>
+  <Actions Context="Author">
+    <Exec>
+      <Command>java.exe</Command>
+      <Arguments>-jar c:\jenkins\swarm-client.jar -autoDiscoveryAddress 192.168.33.255 -labels windows -fsroot c:\jenkins</Arguments>
+    </Exec>
+  </Actions>
+</Task>
+:next2
+
 rem Schedule start of swarm client at start of the machine (after next reboot)
-schtasks /CREATE /TN JenkinsSwarmClient /RU vagrant /RP vagrant /XML "c:\vagrant\scripts\JenkinsSwarmClient.xml"
+rem
+
+schtasks /CREATE /TN JenkinsSwarmClient /RU vagrant /RP vagrant /XML "%TEMP%\JenkinsSwarmClient.xml"
+
+
+::########################################
+::## Here's the heredoc processing code ##
+::########################################
+:heredoc <uniqueIDX>
+setlocal enabledelayedexpansion
+set go=
+for /f "delims=" %%A in ('findstr /n "^" "%~f0"') do (
+  set "line=%%A" && set "line=!line:*:=!"
+  if defined go (if #!line:~1!==#!go::=! (goto :EOF) else echo(!line!)
+  if "!line:~0,13!"=="call :heredoc" (
+    for /f "tokens=3 delims=>^ " %%i in ("!line!") do (
+      if #%%i==#%1 (
+        for /f "tokens=2 delims=&" %%I in ("!line!") do (
+          for /f "tokens=2" %%x in ("%%I") do set "go=%%x"
+        )
+      )
+    )
+  )
+)
+goto :EOF

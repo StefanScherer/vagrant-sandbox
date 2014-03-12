c39c1ff2250f55ad16cd19de13e8ced96c670f60
StefanScherer <scherer_stefan@icloud.com>
2014-03-12 22:12:33 +0100

    set german keyboard

---
+++ win7/shell/set-german.bat
@@ -0,0 +1,55 @@
+rem set timezone to Berlin
+tzutil /s "W. Europe Standard Time"
+
+call :heredoc html >%TEMP%\german.xml && goto next2
+<!-- from http://msdn.microsoft.com/en-ie/goglobal/bb964650(en-us).aspx -->
+<gs:GlobalizationServices xmlns:gs="urn:longhornGlobalizationUnattend">
+
+  <!--User List-->
+  <gs:UserList>
+    <gs:User UserID="Current" CopySettingsToSystemAcct="true" /> 
+  </gs:UserList>
+
+  <!--input preferences--> 
+  <gs:InputPreferences> 
+    <!--en-US--> 
+    <gs:InputLanguageID Action="add" ID="0409:00000409"/> 
+    <!--de-DE--> 
+    <gs:InputLanguageID Action="add" ID="0407:00000407" Default="true"/> 
+  </gs:InputPreferences> 
+
+  <!--location--> 
+  <gs:LocationPreferences> 
+    <gs:GeoID Value="94"/> 
+  </gs:LocationPreferences>
+
+</gs:GlobalizationServices>
+:next2
+echo on
+
+control intl.cpl,, /f:"%TEMP%\german.xml"
+
+goto :EOF
+
+
+::########################################
+::## Here's the heredoc processing code ##
+::########################################
+:heredoc <uniqueIDX>
+@echo off
+setlocal enabledelayedexpansion
+set go=
+for /f "delims=" %%A in ('findstr /n "^" "%~f0"') do (
+  set "line=%%A" && set "line=!line:*:=!"
+  if defined go (if #!line:~1!==#!go::=! (goto :EOF) else echo(!line!)
+  if "!line:~0,13!"=="call :heredoc" (
+    for /f "tokens=3 delims=>^ " %%i in ("!line!") do (
+      if #%%i==#%1 (
+        for /f "tokens=2 delims=&" %%I in ("!line!") do (
+          for /f "tokens=2" %%x in ("%%I") do set "go=%%x"
+        )
+      )
+    )
+  )
+)
+goto :EOF

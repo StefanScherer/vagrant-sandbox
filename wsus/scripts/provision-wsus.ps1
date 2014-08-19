79b6a7cc6e512d9de9004cae1af858dba6d91c07
Stefan Scherer <scherer_stefan@icloud.com>
2014-08-19 10:40:16 +0200

    open ping port for packer-windows builds

--- wsus/scripts/provision-wsus.ps1
+++ wsus/scripts/provision-wsus.ps1
@@ -1,6 +1,9 @@
 # http://smsagent.wordpress.com/2014/02/07/installing-and-configuring-wsus-with-powershell/
 # http://gallery.technet.microsoft.com/Install-and-Configure-WSUS-006a203a
 
+# open ping for packer-windows builds, this deprecated command even works on 2012 R2
+netsh firewall set icmpsetting 8
+
 if (Test-Path 'c:\vagrant\scripts\InstallWSUS.ps1') {
   . c:\vagrant\scripts\InstallWSUS.ps1
 } else {

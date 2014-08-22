42246b3f23cec350c72de915d3644ad19ce6a9bb
Stefan Scherer <scherer_stefan@icloud.com>
2014-08-22 16:16:51 +0200

    simplified WSUS install + configure

--- wsus/scripts/provision-wsus.ps1
+++ wsus/scripts/provision-wsus.ps1
@@ -1,16 +1,5 @@
-# http://smsagent.wordpress.com/2014/02/07/installing-and-configuring-wsus-with-powershell/
-# http://gallery.technet.microsoft.com/Install-and-Configure-WSUS-006a203a
-
 # open ping for packer-windows builds, this deprecated command even works on 2012 R2
 netsh firewall set icmpsetting 8
 
-if (Test-Path 'c:\vagrant\scripts\InstallWSUS.ps1') {
-  . c:\vagrant\scripts\InstallWSUS.ps1
-} else {
-  if (!(Test-Path 'c:\vagrant\resources\InstallWSUS.ps1')) {
-    (New-Object Net.WebClient).DownloadFile("http://gallery.technet.microsoft.com/Install-and-Configure-WSUS-006a203a/file/108277/1/InstallWSUS.ps1", 'c:\vagrant\resources\InstallWSUS.ps1')
-  }
-
-  . c:\vagrant\resources\InstallWSUS.ps1
-}
-
+. C:\vagrant\scripts\install-wsus.ps1
+. C:\vagrant\scripts\configure-wsus.ps1

f9a2ace71163d3a6358e2b3b8860fe4a440ae366
Stefan Scherer <scherer_stefan@icloud.com>
2014-10-13 18:40:07 +0200

    added p50

---
+++ win7-vs2012u3/shell/p50.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget -qO - http://apt.sealsystems.local/repository.pub | sudo apt-key add -
+UBUNTU_VERSION=`lsb_release -c | grep -o "\S*$"` && \
+cat <<APT_SOURCE | sudo tee /etc/apt/sources.list.d/plossys.list
+deb http://apt.sealsystems.local/plossys am5 unstable
+deb http://apt.sealsystems.local/mirror/nodejs $UBUNTU_VERSION main
+APT_SOURCE
+sudo apt-get update -y
+sudo apt-get install plossys -y
